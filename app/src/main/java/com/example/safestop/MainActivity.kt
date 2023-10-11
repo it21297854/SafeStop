36b60a6e458835786aca7cd21f42af532ca44a4d
sabeelmhd <sabeelzaim@gmail.com>
2023-10-11 21:14:49 +0530

    Second Commit

--- app/src/main/java/com/example/safestop/MainActivity.kt
+++ app/src/main/java/com/example/safestop/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.safestop
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        // register all the Buttons with their appropriate IDs
+        val todoB: Button = findViewById(R.id.todoB)
+        val logoutButton: Button = findViewById(R.id.logoutButton)
+
+        // register all the card views with their appropriate IDs
+        val contributeCard: CardView = findViewById(R.id.contributeCard)
+        val practiceCard: CardView = findViewById(R.id.practiceCard)
+        val learnCard: CardView = findViewById(R.id.learnCard)
+        val interestsCard: CardView = findViewById(R.id.interestsCard)
+        val helpCard: CardView = findViewById(R.id.helpCard)
+        val settingsCard: CardView = findViewById(R.id.settingsCard)
+
+
+        // handle each of the buttons with the OnClickListener
+        todoB.setOnClickListener {
+            Toast.makeText(this, "TODO LIST", Toast.LENGTH_SHORT).show()
+        }
+
+        logoutButton.setOnClickListener {
+            mAuth.signOut()
+            Toast.makeText(this, "Session Logging out", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        // handle each of the cards with the OnClickListener
+        contributeCard.setOnClickListener {
+            Toast.makeText(this, "Contribute Articles", Toast.LENGTH_SHORT).show()
+        }
+        practiceCard.setOnClickListener {
+            Toast.makeText(this, "Practice Programming", Toast.LENGTH_SHORT).show()
+        }
+        learnCard.setOnClickListener {
+            Toast.makeText(this, "Learn Programming", Toast.LENGTH_SHORT).show()
+        }
+        interestsCard.setOnClickListener {
+            Toast.makeText(this, "Filter your Interests", Toast.LENGTH_SHORT).show()
+        }
+        helpCard.setOnClickListener {
+            Toast.makeText(this, "Anything Help you want?", Toast.LENGTH_SHORT).show()
+        }
+        settingsCard.setOnClickListener {
+            Toast.makeText(this, "Change the settings", Toast.LENGTH_SHORT).show()
+        }
     }
 }
