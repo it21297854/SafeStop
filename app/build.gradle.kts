0dde35a3044a059189c6ed7105cbda488dd20723
sabeelmhd <sabeelzaim@gmail.com>
2023-10-24 19:22:40 +0530

    Just

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,6 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
     testImplementation("junit:junit:4.13.2")
-//    testImplementation("com.google.truth:truth:1.1.2")
     testImplementation ("com.google.truth:truth:1.1.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
